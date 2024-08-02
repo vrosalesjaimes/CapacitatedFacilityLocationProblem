d1be1d7242bda441a27aef35a28b797386a9d45d
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2024-08-01 19:20:01 -0600

    Creación de constantes para test

---
+++ include/Enum/supplies.h
@@ -0,0 +1,27 @@
+#ifndef SUPPLIES_H
+#define SUPPLIES_H
+
+#include <vector>
+
+using namespace std;
+
+class Supplies {
+    public:
+        const vector<int> PROBLEM_1_BALANCED_VAM = {15,25,10};
+
+        const vector<int> PROBLEM_2_BALANCED_VAM = {80,30,70,45};
+
+        const vector<int> PROBLEM_3_UNBALANCED_VAM = {35,50,40};
+
+        const vector<int> PROBLEM_4_MODI = {70,90,115};
+
+        const vector<int> PROBLEM_5_MODI = {8,10,11};
+
+        const vector<int> PROBLEM_6_MODI = {40,50,40};
+
+        const vector<int> PROBLEM_7_MODI = {8,10,11};
+
+        const vector<int> PROBLEM_8_MODI = {40,50,40};
+};
+
+#endif // SUPPLIES_H
